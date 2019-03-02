25bfd8fb806ba0947066285450f9e03cb74adab4
Barklim <kliment.barkalov@gmail.com>
2019-03-03 01:54:42 +0300

    fix profile_3

--- client/src/components/Admin/profile.js
+++ client/src/components/Admin/profile.js
@@ -68,11 +68,8 @@ class Profile extends Component {
                         
             user.profileGen.map(item => (
                 <tr key={item._id}>
-                    <td>{item.rule_5}</td>
-                    <td>{item.rule_6}</td>
-                    <td>{item.rule_7}</td>
-                    <td>{item.rule_8}</td>
-                    <td>{item.rule_9}</td>
+                    <td>№ 2:</td>
+                    <td>CFTR: 3944delGT: {item.rule_1}</td>
                 </tr>
             ))
         :null
@@ -83,11 +80,8 @@ class Profile extends Component {
                         
             user.profileGen.map(item => (
                 <tr key={item._id}>
-                    <td>{item.rule_10}</td>
-                    <td>{item.rule_11}</td>
-                    <td>{item.rule_12}</td>
-                    <td>{item.rule_13}</td>
-                    <td>{item.rule_14}</td>
+                    <td>№ 3:</td>
+                    <td>CFTR: 3944delGT: {item.rule_0}</td>
                 </tr>
             ))
         :null
@@ -98,11 +92,8 @@ class Profile extends Component {
                         
             user.profileGen.map(item => (
                 <tr key={item._id}>
-                    <td>{item.rule_15}</td>
-                    <td>{item.rule_16}</td>
-                    <td>{item.rule_17}</td>
-                    <td>{item.rule_18}</td>
-                    <td>{item.rule_19}</td>
+                    <td>№ 4:</td>
+                    <td>CFTR: 3944delGT: {item.rule_0}</td>
                 </tr>
             ))
         :null
@@ -113,11 +104,8 @@ class Profile extends Component {
                         
             user.profileGen.map(item => (
                 <tr key={item._id}>
-                    <td>{item.rule_20}</td>
-                    <td>{item.rule_21}</td>
-                    <td>{item.rule_22}</td>
-                    <td>{item.rule_23}</td>
-                    <td>{item.rule_24}</td>
+                    <td>№ 5:</td>
+                    <td>CFTR: 3944delGT: {item.rule_0}</td>
                 </tr>
             ))
         :null
@@ -128,11 +116,8 @@ class Profile extends Component {
                         
             user.profileGen.map(item => (
                 <tr key={item._id}>
-                    <td>{item.rule_25}</td>
-                    <td>{item.rule_26}</td>
-                    <td>{item.rule_27}</td>
-                    <td>{item.rule_28}</td>
-                    <td>{item.rule_29}</td>
+                    <td>№ 6:</td>
+                    <td>CFTR: 3944delGT: {item.rule_0}</td>
                 </tr>
             ))
         :null
@@ -171,63 +156,18 @@ class Profile extends Component {
                     <tbody>
                         {this.showUserPosts(user)}
                     </tbody>
-                    <thead>
-                        <tr>
-                            <th>№ 6</th>
-                            <th>№ 7</th>
-                            <th>№ 8</th>
-                            <th>№ 9</th>
-                            <th>№ 10</th>
-                        </tr>
-                    </thead>
                     <tbody>
                         {this.showUserPosts2(user)}
                     </tbody>
-                    <thead>
-                        <tr>
-                            <th>№ 11</th>
-                            <th>№ 12</th>
-                            <th>№ 13</th>
-                            <th>№ 14</th>
-                            <th>№ 15</th>
-                        </tr>
-                    </thead>
                     <tbody>
                         {this.showUserPosts3(user)}
                     </tbody>
-                    <thead>
-                        <tr>
-                            <th>№ 16</th>
-                            <th>№ 17</th>
-                            <th>№ 18</th>
-                            <th>№ 19</th>
-                            <th>№ 20</th>
-                        </tr>
-                    </thead>
                     <tbody>
                         {this.showUserPosts4(user)}
                     </tbody>
-                    <thead>
-                        <tr>
-                            <th>№ 21</th>
-                            <th>№ 22</th>
-                            <th>№ 23</th>
-                            <th>№ 24</th>
-                            <th>№ 25</th>
-                        </tr>
-                    </thead>
                     <tbody>
                         {this.showUserPosts5(user)}
                     </tbody>
-                    <thead>
-                        <tr>
-                            <th>№ 26</th>
-                            <th>№ 27</th>
-                            <th>№ 28</th>
-                            <th>№ 29</th>
-                            <th>№ 30</th>
-                        </tr>
-                    </thead>
                     <tbody>
                         {this.showUserPosts6(user)}
                     </tbody>
